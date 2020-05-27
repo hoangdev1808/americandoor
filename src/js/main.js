958b9c7710511a8cfc5d03fd24bbf8884e36d842
hoangdev1808 <hoangdev1808@gmail.com>
2020-05-27 16:25:17 +0700

    add code

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,28 @@
 import "@babel/polyfill";
-import U from "./lib/Utilities";
 import Loading from "./lib/Loading";
+
+function bannerHomeSile(){
+	var mySwiper = new Swiper('.slide-home', {
+		spaceBetween: 30,
+		effect: 'fade',
+		speed: 1000,
+		autoplay: {
+			delay: 3000,
+			disableOnInteraction: false,
+		},
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		  },
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+	});
+}
+
 
 document.addEventListener('DOMContentLoaded', () => {
 	Loading();
+	bannerHomeSile();
 });
