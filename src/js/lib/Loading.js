fdd863b8ac44b1ed964a0999ce8faa2f644cd7d9
hoangdev1808 <hoangdev1808@gmail.com>
2020-06-04 16:01:53 +0700

    ladding page

--- src/js/lib/Loading.js
+++ src/js/lib/Loading.js
@@ -1,40 +1,43 @@
 module.exports = () => {
 	return new Promise((resolve, reject) => {
-		let loading = document.getElementById('loading');
-		let progress = document.getElementById('progress');
-		let progressStat = document.getElementById('progstat');
+		let loading = document.getElementById("loading-container");
+		let progressPercentage = loading.querySelector("#progress-percentage");
+		let progressBar = loading.querySelector("#progress-bar");
 		let images = document.images;
 		let imagesLength = images.length;
 		let counter = 0;
 
 		const turnOffLoadingScreen = () => {
-			loading.style.opacity = '0';
-			setTimeout(function() {
-				loading.style.display = 'none';
-				loading.parentNode.removeChild(loading);
-				document.querySelector('body').classList.add('show-page');
-				resolve();
-			}, 1000)
+			if (loading) {
+				loading.style.opacity = "0";
+				setTimeout(function() {
+					loading.parentNode.removeChild(loading);
+					document.querySelector("body").classList.add("show-page");
+					resolve();
+				}, 1000);
+			}
 		};
 
 		const progressing = () => {
-			let n = 100 / imagesLength * (counter += 1);
-			if (progress.style.width = `${n}`, progressStat.innerHTML = `${n}`, counter === imagesLength) {
-				setTimeout(() => {
-					return turnOffLoadingScreen();
-				}, 500);
+			let n = Math.round(100 / imagesLength * (counter += 1));
+			progressBar.style.width = `${n}%`;
+			progressPercentage.innerHTML = `${n}`;
+			if (counter === imagesLength) {
+				return turnOffLoadingScreen();
 			}
 		};
 
-		if (imagesLength === 0) {
-			return turnOffLoadingScreen();
-		} else {
-			for (let i = 0; i < imagesLength; i++) {
-				let img = new Image;
-				img.onload = progressing;
-				img.onerror = progressing;
-				img.src = images[i].src
+		if (loading) {
+			if (imagesLength === 0) {
+				return turnOffLoadingScreen();
+			} else {
+				for (let i = 0; i < imagesLength; i++) {
+					let img = new Image;
+					img.onload = progressing;
+					img.onerror = progressing;
+					img.src = images[i].src;
+				}
 			}
 		}
-	})
+	});
 };
